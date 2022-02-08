a460cd007dc33e9735eeba7a86052c02d466b551
jia1995 <15927447546@163.com>
2022-02-08 18:00:30 +0800

    ref问题修复 存在问题：ref引用依然有问题

--- Hessian2Deserialization.py
+++ Hessian2Deserialization.py
@@ -1,7 +1,11 @@
 import datetime
+from email.mime import base
 import struct
 import base64
 from re import sub
+import json
+
+from pyparsing import withAttribute
 
 def mapTypeJudge(k, v):
     if k!=['keys','values']:
@@ -17,7 +21,7 @@ def mapTypeJudge(k, v):
 class Deserialization2Hessian:
     def __init__(self):
         self.types = []
-        self.refMap = {}
+        self.refMap = []
         self.refId= 0
         self.isLastChunk = True
 
@@ -28,9 +32,9 @@ class Deserialization2Hessian:
         self.len = len(bstr)
         self.pos = 0
         re = self.__decoder__()
-        return re[0]
+        return re #[0]
     
-    def __decoder__(self, gsize=1, size=1):
+    def __decoder__(self, gsize=1, size=1, withType=False):
         if self.pos==self.len:
             return 
         re = []
@@ -59,7 +63,8 @@ class Deserialization2Hessian:
                     re.extend(self.__getObject__(size))
                     size=0
                 elif 0x55<=code<=0x58 or 0x70<=code<=0x7f:
-                    re.append(self.__getList__())
+                    l = self.__getList__(withType)
+                    re.append(l)
                 elif code==0x48 or code==0x4d:
                     re.append(self.__getMap__())
                 elif code==0x5a:
@@ -87,26 +92,23 @@ class Deserialization2Hessian:
         while gsize:
             if self.pos<self.len:
                 code = self.bstr[self.pos]
-                if (code >= 0x80 and code <= 0xbf) or (code >= 0xc0 and code <= 0xcf) or (code >= 0xd0 and code <= 0xd7) or code == 0x49: 
+                if 0x80<=code<=0xbf or 0xc0<=code<=0xcf or 0xd0<=code<=0xd7 or code==0x49: 
                     v.append(self.__getInt__())
                     gsize-=1
-                elif ( 0xd8 <= code <= 0xff)or ( 0x38 <= code <= 0x3f) or (code == 0x59) or (code == 0x4c):
+                elif 0xd8<=code<=0xff or 0x38<=code<=0x3f or code==0x59 or code==0x4c:
                     l = self.__getLong__()
                     v.append(l)
                     gsize-=1
-                elif code==0x44 or (0x5b<=code<=0x5f):
+                elif code==0x44 or 0x5b<=code<=0x5f:
                     v.append(self.__getDouble__())
                     gsize-=1
                 elif code in (0x4a, 0x4b):
                     v.append(self.__getDate__())
                     gsize-=1
-                elif (code >= 0x20 and code <= 0x2f) or (code == 0x41) or (code == 0x42) or (code >= 0x34 and code <= 0x37):
+                elif 0x20<=code<=0x2f or 0x41<=code<=0x42 or 0x34<=code<=0x37:
                     v.append( self.__getBytes__())
                     gsize-=1
-                elif code in (0x00, 0x01, 0x02, 0x03,0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 
-                        0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,0x10, 0x11, 0x12, 0x13, 
-                        0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 
-                        0x1e, 0x1f,0x30, 0x31, 0x32, 0x33,0x53,0x52):
+                elif 0x00<=code<=0x1f or 0x30<=code<=0x33 or 0x52<=code<=0x53:
                     v.append(self.__getString__())
                     gsize-=1
                 elif code==0x43:
@@ -114,11 +116,11 @@ class Deserialization2Hessian:
                     v.append(x)
                     gsize-=1
                 elif code==0x4f or 0x60<=code<=0x6f:
-                    o = self.__getObject__(gsize)
+                    o = self.__getObject__(gsize, withType=withType)
                     v.extend(o)
                     gsize-=len(o)
                 elif 0x55<=code<=0x58 or 0x70<=code<=0x7f:
-                    v.append(self.__getList__())
+                    re.append(self.__getList__(withType))
                     gsize-=1
                 elif code==0x48 or code==0x4d:
                     gsize-=1
@@ -301,10 +303,7 @@ class Deserialization2Hessian:
         str1 = ''
         code = self.bstr[self.pos]
         self.pos+=1
-        if code in [0x00, 0x01, 0x02, 0x03,0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 
-                    0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,0x10, 0x11, 0x12, 0x13, 
-                    0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 
-                    0x1e, 0x1f]:
+        if 0x00<=code<=0x1f:
             self.isLastChunk = True
             length = code - 0x00
             t = self.__bin2Str__(self.bstr[self.pos:self.pos+length])
@@ -366,38 +365,37 @@ class Deserialization2Hessian:
         return types
 
     def __getClass__(self):
+        pos = self.pos
         self.pos+=1
-        code = self.bstr[self.pos]
-        self.pos+=1
-        length=self.bstr[self.pos]-0x00
-        self.pos+=1
-        classes = str(self.bstr[self.pos:self.pos+length],encoding='utf8')
-        self.pos+=length
-        size=self.__getInt__()
-        re = self.__decoder__(size, size)
+        classes=self.__decoder__(gsize=0)[0]
         mt = sub(r'com\.caucho\.hessian\.io\..*Handle','', classes)
+        idx = self.refId
+        self.__addRef__(None)
+        size=self.__getInt__()
+        re = self.__decoder__(size, size, pos==0)
+        self.refMap[idx] = re
         if mt=='':
             re = list(re.values())
             re = re if len(re)>1 else re[0]
         return re
 
-    def __getObject__(self, length=0):
+    def __getObject__(self, length=0, withType=False):
         code = self.bstr[self.pos]
         self.pos+=1
         if code==0x4f:
             length = self.bstr[self.pos]-0x90
             self.pos+=1
-            return self.__decoder__(gsize=0, size=length)
+            return self.__decoder__(gsize=0, size=length, withType=withType)
         elif code>=0x60 and code<=0x6f:
-            re=self.__decoder__(size=length, gsize=0)
+            re=self.__decoder__(size=length, gsize=0, withType=withType)
             return re[0] if len(re)==1 and isinstance(re[0], list) else re
             
     
     def __addRef__(self, obj):
-        self.refMap[self.refId] = obj
+        self.refMap.append(obj)
         self.refId+=1
 
-    def __getList__(self):
+    def __getList__(self, withType=False):
         code = self.bstr[self.pos]
         self.pos+=1
         re = []
@@ -415,7 +413,10 @@ class Deserialization2Hessian:
             length = code-0x70
         elif code>=0x78 and code<=0x7f:
             length = code-0x78
-        return self.__decoder__(size=length, gsize=0)
+        re = self.__decoder__(size=length, gsize=0)
+        if withType:
+            self.__addRef__(re)
+        return re
 
     def __getMapData__(self, maps={}):
         code = self.bstr[self.pos]
@@ -429,7 +430,7 @@ class Deserialization2Hessian:
     def __getRef__(self):
         code = self.bstr[self.pos]
         self.pos +=1
-        lens = self.__decoder__(size=1,gsize=0)
+        lens = self.__decoder__(size=1,gsize=0)[0]
         return self.refMap[lens]
 
     def __getMap__(self):
@@ -451,10 +452,12 @@ if __name__=='__main__':
     enc = 'QzAiY29tLmdlZWtwbHVzLmh5cGVycHVsc2UuSGVzc2lhbkR0b50CcDECcDICcDMCcDQCcDUCcDYCcDcCcDgCcDkDcDEyA3AxMwNwMTADcDExYJGR4eFEP/GZmaAAAABDMCFjb20uY2F1Y2hvLmhlc3NpYW4uaW8uRmxvYXRIYW5kbGWRBl92YWx1ZWFEP/GZmaAAAABfAAAETF8AAARMBHhpeGlUVHIaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3QEeGl4aQRoYWhhcgdbc3RyaW5nBHhpeGkEaGFoYQ=='
     bstr = base64.b64decode(enc)
     deserialization2Hessian = Deserialization2Hessian()
-    # print(deserialization2Hessian.decoder(bstr))
-    print(deserialization2Hessian.decoder(enc))
-    enc = 'QzAzY29tLmdlZWtwbHVzLmhlcGhhZXN0dXMud21zLmNvcmUubW9jay5XbXNNb2NrUmVzdWx0kgZyZXN1bHQEZGF0YWAHU1VDQ0VTUwR4aXhp'
-    print(deserialization2Hessian.decoder(enc))
-    enc = 'QzAiY29tLmdlZWtwbHVzLmh5cGVycHVsc2UuSGVzc2lhbkR0b6ECcDECcDICcDMCcDQCcDUCcDYCcDcCcDgCcDkDcDEyA3AxMwNwMTYDcDE3A3AxMANwMTEDcDE0A3AxNWCRkeHhRD/xmZmgAAAAQzAhY29tLmNhdWNoby5oZXNzaWFuLmlvLkZsb2F0SGFuZGxlkQZfdmFsdWVhRD/xmZmgAAAAXwAABExfAAAETAR4aXhpVFRDMCBjb20uY2F1Y2hvLmhlc3NpYW4uaW8uQnl0ZUhhbmRsZZEGX3ZhbHVlYpGRchpqYXZhLnV0aWwuQXJyYXlzJEFycmF5TGlzdAR4aXhpBGhhaGFyB1tzdHJpbmcEeGl4aQRoYWhhQzA1Y29tLmdvb2dsZS5jb21tb24uY29sbGVjdC5JbW11dGFibGVNYXAkU2VyaWFsaXplZEZvcm2SBGtleXMGdmFsdWVzY3IHW29iamVjdALlvKDkuIkC5p2O5ZubcpIC5YyX5LqsAuS4iua1t0oAAAF+nzt8QQ=='
-    # print(base64.b64decode(enc))
-    print(deserialization2Hessian.decoder(enc))
+    # json.dump(deserialization2Hessian.decoder(enc), open('a1.json','w'), indent=2)
+    # enc = 'QzAzY29tLmdlZWtwbHVzLmhlcGhhZXN0dXMud21zLmNvcmUubW9jay5XbXNNb2NrUmVzdWx0kgZyZXN1bHQEZGF0YWAHU1VDQ0VTUwR4aXhp'
+    # json.dump(deserialization2Hessian.decoder(enc), open('a2.json','w'), indent=2)
+    # enc = 'QzAiY29tLmdlZWtwbHVzLmh5cGVycHVsc2UuSGVzc2lhbkR0b6ECcDECcDICcDMCcDQCcDUCcDYCcDcCcDgCcDkDcDEyA3AxMwNwMTYDcDE3A3AxMANwMTEDcDE0A3AxNWCRkeHhRD/xmZmgAAAAQzAhY29tLmNhdWNoby5oZXNzaWFuLmlvLkZsb2F0SGFuZGxlkQZfdmFsdWVhRD/xmZmgAAAAXwAABExfAAAETAR4aXhpVFRDMCBjb20uY2F1Y2hvLmhlc3NpYW4uaW8uQnl0ZUhhbmRsZZEGX3ZhbHVlYpGRchpqYXZhLnV0aWwuQXJyYXlzJEFycmF5TGlzdAR4aXhpBGhhaGFyB1tzdHJpbmcEeGl4aQRoYWhhQzA1Y29tLmdvb2dsZS5jb21tb24uY29sbGVjdC5JbW11dGFibGVNYXAkU2VyaWFsaXplZEZvcm2SBGtleXMGdmFsdWVzY3IHW29iamVjdALlvKDkuIkC5p2O5ZubcpIC5YyX5LqsAuS4iua1t0oAAAF+nzt8QQ=='
+    # # print(base64.b64decode(enc))
+    # json.dump(deserialization2Hessian.decoder(enc), open('a3.json','w'), indent=2,ensure_ascii=False)
+    enc = "QwR0ZW1wlgFhAWQBZgFsAW0CbTJgyIBfAXUgFEMwIWNvbS5jYXVjaG8uaGVzc2lhbi5pby5GbG9hdEhhbmRsZZEGX3ZhbHVlYURBFHmkYAAAAHQaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3SRkpSVQzA1Y29tLmdvb2dsZS5jb21tb24uY29sbGVjdC5JbW11dGFibGVNYXAkU2VyaWFsaXplZEZvcm2SBGtleXMGdmFsdWVzYnIHW29iamVjdANhZHMDZHNmcpFRknOQyDanyEFicZEDZHNjcZFRkw=='#'QwR0ZW1wlwFhAWQBZgFsAmwyAW0CbTJgyIBfAXUgFEMwIWNvbS5jYXVjaG8uaGVzc2lhbi5pby5GbG9hdEhhbmRsZZEGX3ZhbHVlYURBFHmkYAAAAHQaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3SRkpSVc5DINqfIQUMwNWNvbS5nb29nbGUuY29tbW9uLmNvbGxlY3QuSW1tdXRhYmxlTWFwJFNlcmlhbGl6ZWRGb3JtkgRrZXlzBnZhbHVlc2JyB1tvYmplY3QDYWRzA2RzZnKRUZJRk2JxkQNkc2NxkVGU"
+    print(base64.b64decode(enc))
+    json.dump(deserialization2Hessian.decoder(enc), open('a4.json','w'), indent=2,ensure_ascii=False)

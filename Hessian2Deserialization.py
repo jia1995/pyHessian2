d63e9d9cd66b7feb8e9d9981dc42ba139cef0690
jia1995 <15927447546@163.com>
2022-02-09 23:01:56 +0800

    bug fixing: passed circular reference example TODO: Map data testing

--- Hessian2Deserialization.py
+++ Hessian2Deserialization.py
@@ -29,16 +29,16 @@ class Deserialization2Hessian:
         while size>0:
             if self.pos<self.len:
                 code = self.bstr[self.pos]
-                if (code >= 0x80 and code <= 0xbf) or (code >= 0xc0 and code <= 0xcf) or (code >= 0xd0 and code <= 0xd7) or code == 0x49:
+                if 0x80<=code<=0xbf or 0xc0<=code<=0xcf or 0xd0<=code<=0xd7 or code==0x49:
                     re.append(self.__getInt__())
-                elif ( 0xd8 <= code <= 0xff)or ( 0x38 <= code <= 0x3f) or (code == 0x59) or (code == 0x4c):
+                elif 0xd8<=code<=0xff or 0x38<=code<=0x3f or code==0x59 or code==0x4c:
                     l = self.__getLong__()
                     re.append(l)
-                elif code==0x44 or (0x5b<=code<=0x5f):
+                elif code==0x44 or 0x5b<=code<=0x5f:
                     re.append(self.__getDouble__())
                 elif code in (0x4a, 0x4b):
                     re.append(self.__getDate__())
-                elif (code >= 0x20 and code <= 0x2f) or (code == 0x41) or (code == 0x42) or (code >= 0x34 and code <= 0x37):
+                elif  0x20<=code<=0x2f or code==0x41 or code==0x42 or 0x34<=code<=0x37:
                     re.append( self.__getBytes__())
                 elif 0x00<=code<=0x1f or 0x30<=code<=0x33 or 0x52<=code<=0x53:
                     re.append(self.__getString__())
@@ -83,12 +83,12 @@ class Deserialization2Hessian:
     def __getInt__(self):
         code = self.bstr[self.pos]
         self.pos+=1
-        if code >= 0x80 and code <= 0xbf:
+        if 0x80 <= code <= 0xbf:
             return code - 0x90
-        if code >= 0xc0 and code <= 0xcf:
+        if 0xc0 <= code <= 0xcf:
             self.pos+=1
             return ((code - 0xc8) << 8) + self.bstr[self.pos-1]
-        if code >= 0xd0 and code <= 0xd7:
+        if 0xd0 <= code <= 0xd7:
             b1 = self.bstr[self.pos]
             self.pos+=1
             b0 = self.bstr[self.pos]
@@ -103,22 +103,22 @@ class Deserialization2Hessian:
         self.pos+=1
         if 0xd8 <= code <= 0xef:
             return int(code - 0xe0)
-        elif code >= 0xf0 and code <= 0xff:
+        elif 0xf0 <= code <= 0xff:
             self.pos+=1
             return int(((code - 0xf8) << 8) + self.bstr[self.pos-1])
-        elif code >= 0x38 and code <= 0x3f:
+        elif 0x38 <= code <= 0x3f:
             b1 = self.bstr[self.pos]
             self.pos+=1
             b0 = self.bstr[self.pos]
             self.pos+=1
             return ((code - 0x3c) << 16) + (b1 << 8) + b0
-        elif (code == 0x59):
+        elif code == 0x59:
             b1 = self.bstr[self.pos]
             self.pos+=1
             b0 = self.bstr[self.pos]
             self.pos+=1
             return b1<<8+b0
-        elif (code == 0x4c):
+        elif code == 0x4c:
             re = 0
             for i in range(4):
                 re<<=8
@@ -129,28 +129,28 @@ class Deserialization2Hessian:
     def __getDouble__(self):
         code = self.bstr[self.pos]
         self.pos+=1
-        if (code == 0x44):
+        if code == 0x44:
             re = self.bstr[self.pos:self.pos+8][::-1]
             self.pos+=8
             re = struct.unpack('d', re)[0]
             return re
-        if (code == 0x5b):
+        if code == 0x5b:
             return 0.0
-        if (code == 0x5c):
+        if code == 0x5c:
             return 1.0
-        if (code == 0x5d):
+        if code == 0x5d:
             c = self.bstr[self.pos]
             re = float(int(c)) if c<0x80 else c-0xff
             self.pos +=1
             return re
-        if (code == 0x5e):
+        if code == 0x5e:
             re = 0
             for i in range(2):
                 re<<=8
                 re+=self.bstr[self.pos]
                 self.pos+=1
             return re if re<0x8000 else re-0xffff
-        if (code == 0x5f):
+        if code == 0x5f:
             re = 0
             for i in range(4):
                 re<<=8
@@ -163,13 +163,13 @@ class Deserialization2Hessian:
         code = self.bstr[self.pos]
         self.pos+=1
         re = 0
-        if (code == 0x4a):
+        if code == 0x4a:
             for i in range(8):
                 re<<=8
                 re+=self.bstr[self.pos]
                 self.pos+=1
             return datetime.datetime.strftime(datetime.datetime.fromtimestamp(re/1000),'%Y-%m-%d %H:%M:%S.%f')
-        if (code == 0x4b):
+        if code == 0x4b:
             for i in range(4):
                 re<<=8
                 re+=self.bstr[self.pos]
@@ -179,13 +179,13 @@ class Deserialization2Hessian:
     def __getBytes__(self):
         code = self.bstr[self.pos]
         self.pos+=1
-        if (code >= 0x20 and code <= 0x2f):
+        if 0x20 <= code <= 0x2f:
             lens = code - 0x20
             self.pos+=lens
             return self.bstr[self.pos-lens:self.pos]
         bufs = b''
         # get non-final trunk start with 'A'
-        while (code == 0x41):
+        while code == 0x41:
             length=0
             for i in range(2):
                 length<<=8
@@ -196,8 +196,7 @@ class Deserialization2Hessian:
             code = self.bstr[self.pos]
             self.pos+=1
 
-        if (code == 0x42):
-            # get the last trunk start with 'B'
+        if code == 0x42: # get the last trunk start with 'B'
             length=0
             for i in range(2):
                 length<<=8
@@ -205,11 +204,11 @@ class Deserialization2Hessian:
                 self.pos+=1
             bufs+=self.bstr[self.pos:self.pos+length]
             self.pos+=length
-        elif (code >= 0x20 and code <= 0x2f):
+        elif 0x20 <= code <= 0x2f:
             length = code - 0x20
             bufs+=self.bstr[self.pos:self.pos+length]
             self.pos+=length
-        elif (code >= 0x34 and code <= 0x37):
+        elif 0x34 <= code <= 0x37:
             b1 = self.bstr[self.pos]
             self.pos+=1
             length = (code - 0x34) * 256 + b1
@@ -237,7 +236,7 @@ class Deserialization2Hessian:
                 t = self.__bin2Str__(self.bstr[self.pos:self.pos+length])
             str1 += t
             self.pos+=length
-        elif code in [0x30, 0x31, 0x32, 0x33]:
+        elif 0x30<=code<= 0x33:
             self._isLastChunk = True
             b1 = self.bstr[self.pos]
             self.pos+=1
@@ -281,7 +280,7 @@ class Deserialization2Hessian:
     def __getType__(self):
         code = self.bstr[self.pos]
         types = ''
-        if code in [0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,0x1b,0x1c,0x1d,0x1e,0x1f,0x30,0x31,0x32,0x33,0x52,0x53]:
+        if 0x00<=code <= 0x1f or 0x30<= code <= 0x33 or 0x52<=code<=0x53:
             types = self.__getString__()
             self.types.append(types)
         else:
@@ -289,37 +288,36 @@ class Deserialization2Hessian:
             types = self.types[ref]
         return types
 
-    def __generateClass__(self, classes, k, v, idx=None):
+    def __generateClass__(self, classes, k, v, re, idx=None):
         mt = sub(r'com\.caucho\.hessian\.io\..*Handle','', classes)
-        re = {}
+        res = None
         if 'com.google.common.collect.ImmutableMap' in classes:
             dic = {}
             for a,b in zip(v[0],v[1]):
                 dic[a] = b
-            re = dic
-            self.refMap[idx] = re
+            re.update(dic)
+            res = re
         elif mt=='':
             dic = {}
             for a,b in zip(k,v):
                 dic[a] = b
-            self.refMap[idx] = dic
-            re = v #[0]
+            re.update(dic)
+            res = v
         else:
+            dic = {}
             for a,b in zip(k,v):
-                re[a] = b
-            self.refMap[idx] = re
-        return re
+                dic[a] = b
+            re.update(dic)
+            res = re
+        return res
 
     def __getClass__(self):
         pos = self.pos
         self.pos+=1
         classes=self.__decoder__()[0]
-        
         size=self.__getInt__()
-        a1 = self.pos
         k = self.__decoder__(size)
         self.classes.append({'name':classes, 'fields':k})
-        a2 = self.pos
         v = self.__getObject__(pos==0)
         return v
 
@@ -327,7 +325,8 @@ class Deserialization2Hessian:
         code = self.bstr[self.pos]
         self.pos+=1
         idx = self.refId
-        self.__addRef__(None)
+        res = {}
+        self.__addRef__(res)
         if code==0x4f:
             ref = self.bstr[self.pos]-0x90
             self.pos+=1
@@ -339,7 +338,7 @@ class Deserialization2Hessian:
         for i in fields:
             da = self.__decoder__(withType=withType)
             re.extend(da)
-        return self.__generateClass__(classes, fields, re, idx)
+        return self.__generateClass__(classes, fields, re,res, idx)
             
     
     def __addRef__(self, obj):
@@ -422,7 +421,6 @@ class Deserialization2Hessian:
 
 if __name__=='__main__':
     enc = 'QzAiY29tLmdlZWtwbHVzLmh5cGVycHVsc2UuSGVzc2lhbkR0b50CcDECcDICcDMCcDQCcDUCcDYCcDcCcDgCcDkDcDEyA3AxMwNwMTADcDExYJGR4eFEP/GZmaAAAABDMCFjb20uY2F1Y2hvLmhlc3NpYW4uaW8uRmxvYXRIYW5kbGWRBl92YWx1ZWFEP/GZmaAAAABfAAAETF8AAARMBHhpeGlUVHIaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3QEeGl4aQRoYWhhcgdbc3RyaW5nBHhpeGkEaGFoYQ=='
-    bstr = base64.b64decode(enc)
     deserialization2Hessian = Deserialization2Hessian()
     json.dump(deserialization2Hessian.decoder(enc), open('a1.json','w'), indent=2)
     enc = 'QzAzY29tLmdlZWtwbHVzLmhlcGhhZXN0dXMud21zLmNvcmUubW9jay5XbXNNb2NrUmVzdWx0kgZyZXN1bHQEZGF0YWAHU1VDQ0VTUwR4aXhp'
@@ -434,8 +432,8 @@ if __name__=='__main__':
     enc = "QwR0ZW1wlgFhAWQBZgFsAW0CbTJgyIBfAXUgFEMwIWNvbS5jYXVjaG8uaGVzc2lhbi5pby5GbG9hdEhhbmRsZZEGX3ZhbHVlYURBFHmkYAAAAHQaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3SRkpSVQzA1Y29tLmdvb2dsZS5jb21tb24uY29sbGVjdC5JbW11dGFibGVNYXAkU2VyaWFsaXplZEZvcm2SBGtleXMGdmFsdWVzYnIHW29iamVjdANhZHMDZHNmcpFRknOQyDanyEFicZEDZHNjcZFRkw=='#'QwR0ZW1wlwFhAWQBZgFsAmwyAW0CbTJgyIBfAXUgFEMwIWNvbS5jYXVjaG8uaGVzc2lhbi5pby5GbG9hdEhhbmRsZZEGX3ZhbHVlYURBFHmkYAAAAHQaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3SRkpSVc5DINqfIQUMwNWNvbS5nb29nbGUuY29tbW9uLmNvbGxlY3QuSW1tdXRhYmxlTWFwJFNlcmlhbGl6ZWRGb3JtkgRrZXlzBnZhbHVlc2JyB1tvYmplY3QDYWRzA2RzZnKRUZJRk2JxkQNkc2NxkVGU"
     deserialization2Hessian = Deserialization2Hessian()
     json.dump(deserialization2Hessian.decoder(enc), open('a4.json','w'), indent=2,ensure_ascii=False)
-    # enc = 'QzAiY29tLmdlZWtwbHVzLmhlcGhhZXN0dXMuSGVzc2lhbkR0b6UCcDECcDICcDMCcDQCcDUCcDYCcDcCcDgCcDkDcDEyA3AxMwNwMTYDcDE3A3AxOANwMTkDcDIwA3AxMANwMTEDcDE0A3AxNQNwMjFgkZHh4UQ/8ZmZoAAAAEMwIWNvbS5jYXVjaG8uaGVzc2lhbi5pby5GbG9hdEhhbmRsZZEGX3ZhbHVlYUQ/8ZmZoAAAAF8AAARMXwAABEwEeGl4aVRUQzAgY29tLmNhdWNoby5oZXNzaWFuLmlvLkJ5dGVIYW5kbGWRBl92YWx1ZWKRkdPzt17zUkTBSl9+gAAAAHIaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3QEeGl4aQRoYWhhcgdbc3RyaW5nBHhpeGkEaGFoYUMwNWNvbS5nb29nbGUuY29tbW9uLmNvbGxlY3QuSW1tdXRhYmxlTWFwJFNlcmlhbGl6ZWRGb3JtkgRrZXlzBnZhbHVlc2NyB1tvYmplY3QC5byg5LiJAuadjuWbm3KSAuWMl+S6rALkuIrmtbdKAAABftnHtHtjcpICNDUCMjNyklGVUZU='
-    # json.dump(deserialization2Hessian.decoder(enc), open('a4.json','w'), indent=2,ensure_ascii=False)
-    enc = 'QwpNeUxpbmtMaXN0kgFhBG5leHRgkVGQ'
+    enc = 'QzAiY29tLmdlZWtwbHVzLmhlcGhhZXN0dXMuSGVzc2lhbkR0b6UCcDECcDICcDMCcDQCcDUCcDYCcDcCcDgCcDkDcDEyA3AxMwNwMTYDcDE3A3AxOANwMTkDcDIwA3AxMANwMTEDcDE0A3AxNQNwMjFgkZHh4UQ/8ZmZoAAAAEMwIWNvbS5jYXVjaG8uaGVzc2lhbi5pby5GbG9hdEhhbmRsZZEGX3ZhbHVlYUQ/8ZmZoAAAAF8AAARMXwAABEwEeGl4aVRUQzAgY29tLmNhdWNoby5oZXNzaWFuLmlvLkJ5dGVIYW5kbGWRBl92YWx1ZWKRkdPzt17zUkTBSl9+gAAAAHIaamF2YS51dGlsLkFycmF5cyRBcnJheUxpc3QEeGl4aQRoYWhhcgdbc3RyaW5nBHhpeGkEaGFoYUMwNWNvbS5nb29nbGUuY29tbW9uLmNvbGxlY3QuSW1tdXRhYmxlTWFwJFNlcmlhbGl6ZWRGb3JtkgRrZXlzBnZhbHVlc2NyB1tvYmplY3QC5byg5LiJAuadjuWbm3KSAuWMl+S6rALkuIrmtbdKAAABftnHtHtjcpICNDUCMjNyklGVUZU='
+    json.dump(deserialization2Hessian.decoder(enc), open('a4.json','w'), indent=2,ensure_ascii=False)
+    enc = 'QwpNeUxpbmtMaXN0kgFhBG5leHRgkVGQ' # circular test case
     deserialization2Hessian = Deserialization2Hessian()
-    json.dump(deserialization2Hessian.decoder(enc), open('a5.json','w'), indent=2,ensure_ascii=False)
+    print(deserialization2Hessian.decoder(enc))
